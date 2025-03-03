f70e9bdd2f01b524a714637d5c5a911e1b9d9338
kso <88135327+kimsinok@users.noreply.github.com>
2025-03-03 12:13:14 +0900

    Update WebConfig.java

--- src/main/java/com/example/postapp/config/WebConfig.java
+++ src/main/java/com/example/postapp/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true) // 쿠키 혀용 여부
+                //.allowCredentials(true) // 쿠키 혀용 여부
                 // .exposedHeaders("Authorization") // 노출할 헤더
                 .maxAge(3600); // Preflight 요청 캐싱 시간 (초 단위)
 
